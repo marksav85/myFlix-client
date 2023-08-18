e82ee981f8523b6973c621719ae65f04b8116264
Mark Saville <mark.saville@outlook.com>
2023-08-18 13:30:14 +0200

    fix: Add local storage set item to login

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,27 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+// eslint-disable-next-line react/prop-types
 export const LoginView = ({ onLoggedIn }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault(); //prevents default refresh upon submit
 
     const data = {
       Username: username,
-      Password: password
+      Password: password,
     };
-  
-    fetch('https://my-flix-films-d4434240379d.herokuapp.com/login', {
-      method: 'POST',
+
+    fetch("https://my-flix-films-d4434240379d.herokuapp.com/login", {
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     })
       .then((response) => response.json())
       .then((data) => {
         console.log("Login response: ", data);
         if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
           onLoggedIn(data.user, data.token);
         } else {
           alert("No such user");
@@ -30,7 +33,7 @@ export const LoginView = ({ onLoggedIn }) => {
       .catch((e) => {
         alert("Something went wrong" + e);
       });
-    };
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -39,13 +42,13 @@ export const LoginView = ({ onLoggedIn }) => {
         <input
           type="text"
           value={username}
-          onChange={(e) => setUsername(e.target.value)} 
+          onChange={(e) => setUsername(e.target.value)}
           required
         />
       </label>
       <label>
         Password:
-        <input 
+        <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -54,5 +57,5 @@ export const LoginView = ({ onLoggedIn }) => {
       </label>
       <button type="submit">Submit</button>
     </form>
-  )};
-
+  );
+};
