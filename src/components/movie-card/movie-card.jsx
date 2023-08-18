e953ca5f9b0864f86b072cc374ea1439de113013
Mark Saville <mark.saville@outlook.com>
2023-08-18 13:42:34 +0200

    add bootstrap card to display movie info

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,19 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 export const MovieCard = ({ movies, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movies);
-      }}
-    >
-      {movies.title}
-    </div>
+    <Card>
+      <Card.Img variant="top" src={movies.image} />
+      <Card.Body>
+        <Card.Title>{movies.title}</Card.Title>
+        <Card.Text>{movies.description}</Card.Text>
+        <Card.Text>{movies.genre}</Card.Text>
+        <Card.Text>{movies.director}</Card.Text>
+        <Button variant="link" onClick={() => onMovieClick(movies)}>
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
@@ -16,11 +22,11 @@ MovieCard.propTypes = {
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired
+      Name: PropTypes.string.isRequired,
     }),
     Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired
-    })
+      Name: PropTypes.string.isRequired,
+    }),
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: PropTypes.func.isRequired,
 };
