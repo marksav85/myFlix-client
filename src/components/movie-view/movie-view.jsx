84d306e8d8921fa4d0e3f82edb21329b7bce1917
Mark Saville <mark.saville@outlook.com>
2023-08-24 14:01:46 -0700

    refactor movie view to include router links

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,34 +1,38 @@
+import React from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import "./movie-view.scss";
-export const MovieView = ({ movies, onBackClick }) => {
+
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
+
+  const movie = movies.find((m) => m.id === movieId);
+
   return (
     <div>
       <div>
-        <img src={movies.image} />
+        <img src={movie.image} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movies.title}</span>
+        <span>{movie.title}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movies.director}</span>
+        <span>{movie.director}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movies.genre}</span>
+        <span>{movie.genre}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movies.description}</span>
+        <span>{movie.description}</span>
       </div>
 
-      <button
-        onClick={onBackClick}
-        className="back-button"
-        style={{ cursor: "pointer" }}
-      >
-        Back
-      </button>
+      <Link to={`/`}>
+        <button className="back-button">Back</button>
+      </Link>
     </div>
   );
 };
