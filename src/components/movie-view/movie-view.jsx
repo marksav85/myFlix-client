e9af4d1269757739b99fc6bb00e14eb674f09fa0
Mark Saville <mark.saville@outlook.com>
2023-08-24 18:44:13 -0700

    add and remove favorites to account

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,38 +1,92 @@
+/* eslint-disable react/prop-types */
 import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 import "./movie-view.scss";
+import { Card } from "react-bootstrap";
 
-export const MovieView = ({ movies }) => {
+export const MovieView = ({ movies, user, setUser, token }) => {
   const { movieId } = useParams();
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    const isFavorited = user.FavoriteMovies.includes(movieId);
+    setIsFavorite(isFavorited);
+  }, []);
+
+  const removeFavorite = () => {
+    fetch(
+      `https://my-flix-films-d4434240379d.herokuapp.com/users/${user.Username}/${movieId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          setIsFavorite(false);
+          localStorage.setItem("user", JSON.stringify(data));
+          setUser(data);
+        }
+      });
+  };
+
+  const addToFavorite = () => {
+    fetch(
+      `https://my-flix-films-d4434240379d.herokuapp.com/users/${user.Username}/${movieId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          setIsFavorite(true);
+          localStorage.setItem("user", JSON.stringify(data));
+          setUser(data);
+        }
+      });
+  };
 
   const movie = movies.find((m) => m.id === movieId);
 
   return (
-    <div>
-      <div>
-        <img src={movie.image} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.title}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.director}</span>
-      </div>
-      <div>
-        <span>Genre: </span>
-        <span>{movie.genre}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
-      </div>
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.description}</Card.Text>
+        <Card.Text>{movie.genre}</Card.Text>
+        <Card.Text>{movie.director}</Card.Text>
+      </Card.Body>
+
+      {isFavorite ? (
+        <Button onClick={removeFavorite}>Remove from favorites</Button>
+      ) : (
+        <Button onClick={addToFavorite}>Add to favorites</Button>
+      )}
 
       <Link to={`/`}>
         <button className="back-button">Back</button>
       </Link>
-    </div>
+    </Card>
   );
 };
